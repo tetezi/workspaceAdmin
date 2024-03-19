b3254bb78aa929d611d7ae662270de08c4c0a3f5
wxw <1913483156@qq.com>
2024-03-19 09:47:09 +0800

    配置环境

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,27 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+export default ({ mode }: ConfigEnv): UserConfig => {
+  const { VITE_BASE_PATH, VITE_OUT_DIR } = loadEnv(mode, process.cwd())
+
+  return {
+    base: VITE_BASE_PATH,
+    plugins: [vue(), vueJsx()],
+    server: {
+      host: true
+    },
+    build: {
+      // sourcemap: "hidden",
+      outDir: VITE_OUT_DIR
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
     }
   }
-})
+}
