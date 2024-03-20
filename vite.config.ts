c1bcf79b8ad59af82d6528d54bfc21f84ffc74af
wxw <1913483156@qq.com>
2024-03-20 15:02:27 +0800

    添加自动导入element

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { ConfigEnv, defineConfig, loadEnv, UserConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueJsx from '@vitejs/plugin-vue-jsx'
-
+import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import vueJsx from "@vitejs/plugin-vue-jsx";
+import AutoImport from "unplugin-auto-import/vite";
+import Components from "unplugin-vue-components/vite";
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfig => {
-  const { VITE_BASE_PATH, VITE_OUT_DIR } = loadEnv(mode, process.cwd())
+  const { VITE_BASE_PATH, VITE_OUT_DIR } = loadEnv(mode, process.cwd());
 
   return {
     base: VITE_BASE_PATH,
-    plugins: [vue(), vueJsx()],
+    plugins: [
+      vue(),
+      vueJsx(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
     server: {
-      host: true
+      host: true,
     },
     build: {
       // sourcemap: "hidden",
-      outDir: VITE_OUT_DIR
+      outDir: VITE_OUT_DIR,
     },
     resolve: {
       alias: {
-        '@': fileURLToPath(new URL('./src', import.meta.url))
-      }
-    }
-  }
-}
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+  };
+};
