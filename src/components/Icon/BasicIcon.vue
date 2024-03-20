7b0797ffd93fd2929031f07845a4452b8c204a10
wxw <1913483156@qq.com>
2024-03-20 16:33:00 +0800

    添加icon组件

---
+++ src/components/Icon/BasicIcon.vue
@@ -0,0 +1,29 @@
+<template>
+    <ElIcon :size="size" :class="props.loading ? 'is-loading' : ''">
+        <component :is="icon" />
+    </ElIcon>
+</template>
+<script lang="tsx" setup>
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import { isString } from 'lodash';
+import { computed, type Component } from 'vue';
+defineOptions({
+    name: 'BasicIcon'
+})
+const props = defineProps<{
+    icon: string | Component,
+    size?: string | number
+    loading?: boolean
+}>()
+
+const icon = computed(() => {
+    if (props.loading) {
+        return ElementPlusIconsVue['Loading']
+    } else if (isString(props.icon)) {
+        const icons: any = ElementPlusIconsVue
+        return icons[props.icon]
+    } else {
+        return props.icon
+    }
+})
+</script>
