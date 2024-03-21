af89959417aa45a6605e2553fd98d4ee0f0fcdb0
wxw <1913483156@qq.com>
2024-03-21 09:01:58 +0800

    删除空导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { ConfigEnv, defineConfig, loadEnv, UserConfig } from "vite";
+import { ConfigEnv, loadEnv, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
