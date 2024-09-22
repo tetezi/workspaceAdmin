71ca7c586a2f0c25f786b6d2b038db12cf4e83e1
wwx <1913483156@qq.com>
2024-09-22 16:23:05 +0800

    <feat>添加scss的element导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath, URL } from "node:url";
-
 import { ConfigEnv, loadEnv, UserConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfig => {
   const { VITE_BASE_PATH, VITE_OUT_DIR } = loadEnv(mode, process.cwd());
@@ -19,7 +19,9 @@ export default ({ mode }: ConfigEnv): UserConfig => {
         resolvers: [ElementPlusResolver()],
       }),
       Components({
-        resolvers: [ElementPlusResolver()],
+        resolvers: [ElementPlusResolver({
+          importStyle: "sass",
+        })],
         include: [/\.vue$/, /\.tsx?$/],
       }),
     ],
@@ -33,6 +35,13 @@ export default ({ mode }: ConfigEnv): UserConfig => {
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@use "@/styles/element/index.scss" as *;`,
+        },
       },
     },
   };
