094900265d62cf7f6554a94eb74e823c2e009be7
wwx <1913483156@qq.com>
2024-09-22 17:19:57 +0800

    <feat>枚举管理页面

---
+++ src/views/sys/admin/enum/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <div style="height: 100%;box-sizing: border-box;padding:8px  ">
+        <EnumTableComp style="padding:8 16px">
+        </EnumTableComp>
+        <SaveEnumDialogComp>
+        </SaveEnumDialogComp>
+    </div>
+
+</template>
+<script lang="tsx" setup>
+import { DelEnumCategory, GetEnumCategorys, SaveEnumCategory, type SaveEnumCategoryParamsType } from '@/api/sys/enum';
+import { BasicButton, useDialogForm, useTable } from 'ttz-ui';
+import { pick } from 'lodash';
+const [EnumTableComp, enumTableMethods] = useTable({
+    columns: [
+        { prop: 'name', label: '名称' },
+        { prop: 'title', label: '标题' },
+        {
+            prop: 'details', label: '详情', formatter: (row, details) => {
+                return details.map((detail) => {
+                    return <el-tag>{detail.name}</el-tag>
+                })
+            }
+        },
+        { prop: 'description', label: '备注' },
+        { prop: 'createdAt', label: '创建时间' },
+        { prop: 'updatedAt', label: '修改时间' },
+    ],
+    api: async (_params, pageParams) => {
+        return await GetEnumCategorys(pageParams)
+    },
+    actionColumn: (row) => {
+        return <div>
+            <BasicButton func={() => edit(row)}> 编辑 </BasicButton>
+            <BasicButton func={() => del(row)} type='danger' isConfirm> 删除 </BasicButton>
+        </div>
+    },
+    headerActionRender: <BasicButton func={add} type='primary' > 新增 </BasicButton>,
+    title: `角色管理表`,
+})
+const [SaveEnumDialogComp, saveEnumDialogMethods] = useDialogForm<SaveEnumCategoryParamsType>({
+    labelPosition: 'top',
+    formSchemas: [
+        { field: 'name', component: 'Input', colProps: { span: 12 }, label: '名称（英）', componentProps: { placeholder: '请输入英文名称' } },
+        { field: 'title', component: 'Input', colProps: { span: 12 }, label: '标题（中）', componentProps: { placeholder: '请输入中文标题' } },
+        {
+            field: 'details', component: 'EditableTable', label: '详情', componentProps: () => {
+                return {
+                    columns: [
+                        {
+                            prop: 'name', label: '名称', editConfig: {
+                                component: 'Input'
+                            }
+                        }, {
+                            prop: 'value', label: '值', editConfig: {
+                                component: 'Input'
+                            }
+                        }, {
+                            prop: 'description', label: '备注', editConfig: {
+                                component: 'Input'
+                            }
+                        }
+                    ]
+                }
+            }
+        },
+        { field: 'description', component: 'Input', label: '备注', componentProps: { type: 'textarea', autosize: { minRows: 5, maxRows: 5 } } },
+    ],
+    title: `枚举管理`,
+    closeOnClickModal: false,
+    submitApi: SaveEnumCategory,
+    onClose: () => {
+        enumTableMethods.reload()
+    }
+})
+async function add() {
+    return await saveEnumDialogMethods.open({
+        name: '',
+        title: '',
+        details: [],
+    })
+}
+async function edit(row) {
+    return await saveEnumDialogMethods.open({
+        id: row.id,
+        name: row.name,
+        title: row.title,
+        details: row.details.map((detail) => pick(detail, ['id', 'name', 'value', 'description'])),
+        description: row.description,
+    })
+}
+async function del(row) {
+    return await DelEnumCategory(row.id)
+}
+</script>
