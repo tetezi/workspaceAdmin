1becd2ed3ed0eb1d88f224c25fcad05a83cddf29
wwx <1913483156@qq.com>
2024-09-26 14:06:23 +0800

    <chore>添加vite-plugin-vue-devtools插件依赖

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vueJsx from "@vitejs/plugin-vue-jsx";
 import AutoImport from "unplugin-auto-import/vite";
 import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default ({ mode }: ConfigEnv): UserConfig => {
@@ -24,6 +25,7 @@ export default ({ mode }: ConfigEnv): UserConfig => {
         })],
         include: [/\.vue$/, /\.tsx?$/],
       }),
+      vueDevTools(),
     ],
     server: {
       host: true,
