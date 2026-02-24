b551b3f0fcce24479e1b796c4bd43a22f5e801cf
tetezi <MaHouShouJoTetezi@foxmail.com>
2026-02-24 18:06:43 +0800

    style 添加序号显示

--- src/views/sys/admin/menu/components/MenuTree.vue
+++ src/views/sys/admin/menu/components/MenuTree.vue
@@ -1,73 +1,87 @@
 <template>
-    <div style="display: flex">
-        <el-input v-model="filterText" @change="search" placeholder='输入菜单名称搜索' />
-        <!-- <BasicButton icon="Refresh" :func="reloadMenuTree" tip='重置搜索条件'> </BasicButton> -->
-    </div>
-    <el-tree ref='treeRef' v-bind="treeBind" v-loading="loadingRef">
-    </el-tree>
+  <div style="display: flex">
+    <el-input
+      v-model="filterText"
+      @change="search"
+      placeholder="输入菜单名称搜索"
+    />
+    <!-- <BasicButton icon="Refresh" :func="reloadMenuTree" tip='重置搜索条件'> </BasicButton> -->
+  </div>
+  <el-tree ref="treeRef" v-bind="treeBind" v-loading="loadingRef">
+    <template v-slot="{ data }"> {{ data.sort }}.{{ data.name }} </template>
+  </el-tree>
 </template>
 <script lang="ts" setup>
-import { computed, unref, ref, nextTick } from 'vue';
-import { BasicButton, useApi } from 'ttz-ui';
-import { GetAllMenu } from '@/api/sys/menus';
-import { onClickOutside } from '@vueuse/core'; 
+import { computed, unref, ref, nextTick } from "vue";
+import { BasicButton, useApi } from "ttz-ui";
+import { GetAllMenu } from "@/api/sys/menus";
+import { onClickOutside } from "@vueuse/core";
 const props = defineProps<{
-    selectType: 'check' | 'select'
-}>()
+  selectType: "check" | "select";
+}>();
 // const appId = getEnv("VITE_APP_ID")
 async function reloadMenuTree() {
-    filterText.value = ''
-    await fetch()
+  filterText.value = "";
+  await fetch();
 }
-const { dataRef, loadingRef, fetch } = useApi({
+const { dataRef, loadingRef, fetch } = useApi(
+  {
     api: GetAllMenu,
     immediate: true,
     defaultData: [],
-}, {})
-const treeRef = ref()
-const filterText = ref('')
+  },
+  {},
+);
+const treeRef = ref();
+const filterText = ref("");
 const treeBind = computed(() => {
-    return {
-        nodeKey: 'id',
-        data: unref(dataRef),
-        highlightCurrent: props.selectType === 'select',
-        defaultExpandAll: true,
-        checkOnClickNode: true,
-        showCheckbox: props.selectType === 'check',
-        props: {
-            label: 'name',
-            children: 'subMenus'
-        },
-        filterNodeMethod: (value, data) => {
-            if (!value) return true
-            if (data === unref(treeRef).getCurrentNode()) {
-                return true
-            }
-            return data.name.includes(value)
-        },
-    }
-})
+  return {
+    nodeKey: "id",
+    data: unref(dataRef),
+    highlightCurrent: props.selectType === "select",
+    defaultExpandAll: true,
+    checkOnClickNode: true,
+    showCheckbox: props.selectType === "check",
+    props: {
+      label: "name",
+      children: "subMenus",
+    },
+    filterNodeMethod: (value, data) => {
+      if (!value) return true;
+      if (data === unref(treeRef).getCurrentNode()) {
+        return true;
+      }
+      return data.name.includes(value);
+    },
+  };
+});
 onClickOutside(treeRef, () => {
-    if (props.selectType === 'select') {
-        setTimeout(() => {
-            unref(treeRef)?.setCurrentKey(null)
-        }, 100);
-    }
-})
+  if (props.selectType === "select") {
+    setTimeout(() => {
+      unref(treeRef)?.setCurrentKey(null);
+    }, 100);
+  }
+});
 
 function search(value) {
-    unref(treeRef)!.filter(value)
+  unref(treeRef)!.filter(value);
 }
 function getCurrent() {
-    return unref(treeRef).getCurrentNode()
+  return unref(treeRef).getCurrentNode();
 }
 function getChecked() {
-    return [...unref(treeRef).getCheckedNodes(), ...unref(treeRef).getHalfCheckedNodes()]
+  return [
+    ...unref(treeRef).getCheckedNodes(),
+    ...unref(treeRef).getHalfCheckedNodes(),
+  ];
 }
 function setChecked(keys) {
-    return unref(treeRef).setCheckedKeys(keys, true)
+  return unref(treeRef).setCheckedKeys(keys, true);
 }
 defineExpose({
-    getCurrent, getChecked, setChecked, reloadMenuTree
-})
+  getCurrent,
+  getChecked,
+  setChecked,
+  reloadMenuTree,
+});
 </script>
